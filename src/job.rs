d9506ce07cdeccb55636393272835fab19efa8ab
Casey Rodarmor <casey@rodarmor.com>
2025-03-01 16:52:33 -0800

    Test Job::Replace deserialization

--- src/job.rs
+++ src/job.rs
@@ -3,12 +3,16 @@ use {super::*, replace::Replace};
 mod replace;
 
 #[derive(Deserialize)]
-#[serde(deny_unknown_fields)]
+#[serde(deny_unknown_fields, rename_all = "kebab-case", tag = "type")]
 pub(crate) enum Job {
   Replace(Replace),
 }
 
 impl Job {
+  pub(crate) fn load(path: &Path) -> Result<Self> {
+    Ok(serde_yaml::from_reader(File::open(path)?)?)
+  }
+
   pub(crate) async fn run(self) -> Result {
     match self {
       Self::Replace(replace) => replace.run().await?,
